904c6d415770af065c64b4d9233991e8b6c4604a
hurikake06 <hurikake.oracle@gmail.com>
2019-07-08 16:43:13 +0000

    refact

--- scraping/aeonpet.py
+++ scraping/aeonpet.py
@@ -3,31 +3,18 @@ from bs4 import BeautifulSoup
 import xml.etree.ElementTree as ET
 import re
 
-from MyScraping import MyScraping
-
-
 url_base = "https://www.aeonpet-memorial.com/"
 url_sitemap = url_base + "sitemap.xml"
 url = "https://www.aeonpet-memorial.com/reien/satu-petyasuragi-sapporo/"
 
-url_array = []
+urls = []
 
 with urllib.request.urlopen(url=url_sitemap) as xml_string:
     sitemap = ET.fromstring(xml_string.read())
 
-for child in sitemap.findall('./atom:url', {'atom': 'http://www.sitemaps.org/schemas/sitemap/0.9'}):
-    url = child[0].text
-    if re.match('https://www.aeonpet-memorial.com/reien/.+', url):
-        url_array.append(url)
+urls = [child[0].text for child in sitemap.findall('./atom:url', {'atom': 'http://www.sitemaps.org/schemas/sitemap/0.9'}) if re.match('https://www.aeonpet-memorial.com/reien/.+', child[0].text)]
 
-myScraping = MyScraping(url_array)
+pages = [BeautifulSoup(urllib.request.urlopen(url=url), "html.parser") for url in urls]
 
-for index, page in enumerate(myScraping.pages):
+for index, page in enumerate(pages):
     print('{0}:{1}'.format(index, page.find("div", class_="dethead").find("h1").string))
-
-
-
-
-
-
-
