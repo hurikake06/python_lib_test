fe9e441d3ce1ccfd1646409e2d14ad7ab87737aa
honda.ryuto <honda.ryuto@a-tm.co.jp>
2019-07-09 23:37:03 +0900

    sitemap から取得

--- aeonpet/aeonpet/spiders/scrapy_aeonpet_spider.py
+++ aeonpet/aeonpet/spiders/scrapy_aeonpet_spider.py
@@ -1,14 +1,24 @@
 # -*- coding: utf-8 -*-
 import scrapy
+from scrapy.spiders import SitemapSpider
+from scrapy.selector import Selector
+from aeonpet.items import AeonpetItem
 
-
-class ScrapyAeonpetSpiderSpider(scrapy.Spider):
+class ScrapyAeonpetSpiderSpider(SitemapSpider):
     name = 'scrapy_aeonpet_spider'
     allowed_domains = ['www.aeonpet-memorial.com']
-    start_urls = ['http://www.aeonpet-memorial.com/sitemap.xml']
+    sitemap_urls = ['http://www.aeonpet-memorial.com/sitemap.xml']
     sitemap_rules = [
-        (r'/https://www.aeonpet-memorial.com/reien/.+/', 'parse'),
+        (r'https://www.aeonpet-memorial.com/reien/.+', 'parse'),
     ]
 
     def parse(self, response):
-        self.name = "response.css('.dethead').extract_first().strip()"
+        print('-----------------------')
+        print("URL:", response.url)
+        print('-----------------------')
+        #self.name = "response.css('.dethead').extract_first().strip()"
+        item = AeonpetItem()
+        sel = Selector(response)
+        item['name'] = sel.xpath('//h1').extract_first()
+
+        yield item
